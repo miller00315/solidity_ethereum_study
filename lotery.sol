468aaaabad11d17280d6b18454cc0a03eff883c5
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2021-04-04 13:17:50 -0300

    Ajustando proteçoes

--- lotery.sol
+++ lotery.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Lotery {
     uint256 prizeDrawNumber;
     address owner;
-    uint256 prizeDrawCount = 0;
+    uint16 prizeDrawCount = 0;
     bool richOwner = false;
 
     constructor(uint256 initialValue) {
@@ -27,8 +27,12 @@ contract Lotery {
      *@dev set the prizeDrawNumber
      *@param sended new value of prizeDrawNumber
      */
-    function set(uint256 sended) public payable withMinValue(1000) {
-        prizeDrawCount++;
+    function set(uint16 sended) public payable withMinValue(1000) {
+        require(
+            msg.sender == owner,
+            "Only the contract's owner can set the value"
+        );
+
         prizeDrawNumber = sended;
 
         if (msg.value > 1000) {
@@ -39,11 +43,13 @@ contract Lotery {
 
             emit SendedAmount(msg.sender, amount);
         }
+
+        prizeDrawCount++;
     }
 
     /**
      *@dev withMinValue check if is sended the min value of ether
-     * @param minValue minimun value of ether
+     *@param minValue minimun value of ether
      **/
     modifier withMinValue(uint256 minValue) {
         require(msg.value >= minValue, "Not enough ether was sent");
@@ -60,7 +66,7 @@ contract Lotery {
     /**
      *@dev getCount return the count of preize draw
      */
-    function getCount() public view returns (uint256) {
+    function getCount() public view returns (uint16) {
         return prizeDrawCount;
     }
 
@@ -76,5 +82,27 @@ contract Lotery {
      */
     function fetOwnerRich() public view returns (bool) {
         return richOwner;
+    }
+
+    function getRealatory()
+        public
+        view
+        returns (
+            bool _richOwner,
+            address _owner,
+            uint16 _priceDrawCount,
+            uint256 _prizeDrawNumber,
+            address _thisAddress,
+            uint256 _balance
+        )
+    {
+        return (
+            richOwner,
+            owner,
+            prizeDrawCount,
+            prizeDrawNumber,
+            address(this),
+            address(this).balance
+        );
     }
 }
